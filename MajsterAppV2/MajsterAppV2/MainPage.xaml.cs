4b6dcd9b06a3ef579758374c7f35d76ad6b8750a
Michal <mich.jak2000@gmail.com>
2021-11-14 19:41:04 +0100

    apkav02

---
+++ MajsterAppV2/MajsterAppV2/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace MajsterAppV2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new latarka());
+        }
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new poziomica());
+        }
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new minutnik());
+        }
+    }
+}
