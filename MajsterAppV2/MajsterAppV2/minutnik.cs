131ce40eb26c2b41fc446657a0e01d5edc175931
SzymonZwolinski <szymzwoo@gmail.com>
2021-11-14 21:06:43 +0100

    Minutnik-Nie dziala

--- MajsterAppV2/MajsterAppV2/minutnik.cs
+++ MajsterAppV2/MajsterAppV2/minutnik.cs
@@ -40,6 +40,87 @@ namespace MajsterAppV2
 
                 }
             };
+
+            using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MajsterAppV2
+{
+    public class Page1 : ContentPage
+    {
+
+        public void Funkcja_Minutnik()
+        {
+            Title = "Minutnik";
+            Label label = new Label
+            {
+                Text = "Włącz minutnik",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand
+            };
+            Button przyciskON = new Button
+            {
+                Text = "Włacz",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand
+
+            };
+            Button przyciskOFF = new Button
+            {
+                Text = "Wylacz",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand
+            };
+
+
+
+        
+   
+        public void Minutnik()
+        {
+            Device.StartTimer(TimeSpan.FromSeconds(1), CzasMiniony); // Co sekunde wywoluje funkcje CzasMiniony
+        }
+        public bool CzasMiniony()
+        {
+           
+            int sekunda = 0;
+            int minuta = 0;
+            int godzina = 0;
+
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                sekunda += 1;
+                if (sekunda == 60)
+                {
+                    sekunda = 0;
+                    minuta++;
+                    if (minuta == 60)
+                    {
+                        godzina++;
+                        minuta = 0;
+                    }
+                }
+                Label czas = new Label
+                {
+                    Text = "Godzina: " + godzina + " Minuty:  " + minuta + " Sekundy: " + sekunda, //wyswietla czas co wywolanie funkjci
+                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand
+                 };
+
+
+            });
+            return true; //True = dziala dalej false - stop
+
+
+        }
+
+    }
+}
         }
     }
 }
