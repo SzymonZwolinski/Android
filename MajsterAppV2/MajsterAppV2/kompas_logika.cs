fc423801934ea377ef8a235b47258276a1a9330b
SzymonZwolinski <92259367+SzymonZwolinski@users.noreply.github.com>
2021-11-28 15:13:11 +0100

    Kompas do testu  z opisami

--- MajsterAppV2/MajsterAppV2/kompas_logika.cs
+++ MajsterAppV2/MajsterAppV2/kompas_logika.cs
@@ -15,7 +15,9 @@ namespace MajsterAppV2
 
         private void Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
         {
-            WezWartoscKompasu.Invoke(this, e.Reading.HeadingMagneticNorth.ToString());
+            double value = e.Reading.HeadingMagneticNorth;
+            string valueFormat = string.Format("{0} {1} {2}", value.ToString("0"), CalculateDirection(value), "\u00b0");
+            WezWartoscKompasu?.Invoke(this, valueFormat);
         }
 
         public void start()
@@ -31,6 +33,47 @@ namespace MajsterAppV2
             catch(Exception ex)
             {
                 Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        private string CalculateDirection(double degree)
+        {
+            if (degree < 22)
+            {
+                return "Północ";
+            }
+            else if (degree < 67)
+            {
+                return "Północny wschód";
+            }
+            else if (degree < 112)
+            {
+                return "Wschód";
+            }
+            else if (degree < 157)
+            {
+                return "Południowy wschód";
+            }
+            else if (degree < 202)
+            {
+                return "Południe";
+            }
+            else if (degree < 247)
+            {
+                return "Południowy zachód";
+            }
+            else if (degree < 292)
+            {
+                return "Zachód";
+            }
+            else if (degree < 337)
+            {
+                return "Północny zachód";
+            }
+            else
+            {
+                return "Północ";
             }
         }
 
