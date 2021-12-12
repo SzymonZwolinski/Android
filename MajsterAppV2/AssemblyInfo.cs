5fb2bd457c20045c72826a0333de618e00125a61
MichalJakubczak <mich.jak2000@gmail.com>
2021-12-12 17:40:19 +0100

    Revert "Kątownik Pierwsze podejscie {NO error edition}"
    This reverts commit b89d0f01f5f56e18c4eb99a300755cd3d64e425c.

--- MajsterAppV2/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using Xamarin.Forms.Xaml;
-
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-//[assembly: UsesPermission(Android.Manifest.Permission.Flashlight)]
-//[assembly: UsesPermission(Android.Manifest.Permission.Camera)]
-//[assembly: UsesFeature("android.hardware.camera", Required = false)]
-//[assembly: UsesFeature("android.hardware.camera.autofocus", Required = false)]
