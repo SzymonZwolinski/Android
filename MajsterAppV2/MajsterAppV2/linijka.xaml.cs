61ec0f54044dd03cf2b188c92878199ac41fac20
SzymonZwolinski <92259367+SzymonZwolinski@users.noreply.github.com>
2021-12-14 17:04:11 +0100

    Linijka SKIA

--- MajsterAppV2/MajsterAppV2/linijka.xaml.cs
+++ MajsterAppV2/MajsterAppV2/linijka.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,8 +16,11 @@ namespace MajsterAppV2
         public linijka()
         {
             InitializeComponent();
-            ProgressBar progressBar = new ProgressBar { Progress = 0.9f };
-         
+            Device.StartTimer(TimeSpan.FromSeconds(1f / 60), () =>
+            {
+                canvasView.InvalidateSurface();
+                return true;
+            });
         }
 
         private async void NavigateButton_OnClicked(object sender, EventArgs e)
@@ -24,6 +28,43 @@ namespace MajsterAppV2
             await Navigation.PushAsync(new MainPage());
         }
 
+        readonly SKPaint magentaPaint = new SKPaint
+        {
+            Style = SKPaintStyle.Fill,
+            Color = SKColors.Magenta,
+            IsAntialias = true
+        };
+
+
+        readonly SKPaint redPaint = new SKPaint
+        {
+            Style = SKPaintStyle.StrokeAndFill,
+            Color = SKColors.Red,
+            IsAntialias = true
+        };
+
+        readonly SKPaint greenPaint = new SKPaint
+        {
+            Color = SKColors.Green,
+            Style = SKPaintStyle.Fill,
+            IsAntialias = true
+        };
+        void canvasView_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            var surface = e.Surface;
+            var canvas = surface.Canvas;
+
+            canvas.Clear(SKColors.AliceBlue);
+
+            var width = e.Info.Width;
+            var height = e.Info.Height;
+
+
+            //mm = 3.77957517575 , 5 = (18.89787587875) 10 = 37.7957517575
+
+
+
+        }
 
     }
 
