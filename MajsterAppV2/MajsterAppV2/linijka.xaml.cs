de8a8f2657f9cf54f83e5bf36c888ee2b25a5811
SzymonZwolinski <92259367+SzymonZwolinski@users.noreply.github.com>
2021-12-20 16:10:21 +0100

    Linijka malowanie jakby działające

--- MajsterAppV2/MajsterAppV2/linijka.xaml.cs
+++ MajsterAppV2/MajsterAppV2/linijka.xaml.cs
@@ -32,6 +32,7 @@ namespace MajsterAppV2
         {
             Style = SKPaintStyle.Fill,
             Color = SKColors.Magenta,
+            TextSize = 1.5f,
             IsAntialias = true
         };
 
@@ -40,6 +41,7 @@ namespace MajsterAppV2
         {
             Style = SKPaintStyle.StrokeAndFill,
             Color = SKColors.Red,
+            TextSize = 1.5f,
             IsAntialias = true
         };
 
@@ -47,6 +49,7 @@ namespace MajsterAppV2
         {
             Color = SKColors.Green,
             Style = SKPaintStyle.Fill,
+            TextSize = 1.5f,
             IsAntialias = true
         };
         void canvasView_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
@@ -61,32 +64,42 @@ namespace MajsterAppV2
 
 
             //mm = 3.77957517575 , 5 = (18.89787587875) 10 = 37.7957517575
-
-
+            Maluj(height, canvas);
 
         }
 
         #region Malowanie
-        private void Maluj(int Wysokosc, int Szerokosc)
+        private void Maluj(int Wysokosc, SKCanvas canva)
         {
+            canva.Translate(0, 0);
+            float x = 10;
             double mm_pixel = 3.77957517575;
-            double zakres = Math.Floor( Wysokosc / mm_pixel);
+            double zakres = Math.Floor(Wysokosc / mm_pixel) ;
             int i = 0; // ilosc kresek
-            while(zakres > 0)
+            float[] temp_tab = new float[(int)zakres];
+            for(int temp =0; temp != zakres;temp++)
+            {
+                temp_tab[temp] = (temp+1)/10;
+                
+            }
+            while (zakres > 0)
             {
                 if (i % 4 == 0)
                 {
-                    //5mm
+                    //5mm zielony
+                    canva.DrawText(temp_tab[i].ToString(), x, (float)mm_pixel, greenPaint);
                 }
                 else if(i % 9 == 0)
                 {
-                    //10mm
+                    //10mm madżenta 
+                    canva.DrawText(temp_tab[i].ToString(), x, (float)mm_pixel, magentaPaint);
                 }
                 else
                 {
-                    //1mm
+                    //1mm czerw
+                    canva.DrawText(temp_tab[i].ToString(), x, (float)mm_pixel, redPaint);
                 }
-
+                mm_pixel += mm_pixel;
                 zakres--;
             }
         }
